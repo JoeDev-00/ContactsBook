d14a1ab8b2cafa6bc4dbe803b6537fe10d8be497
Joseph Dieudonné ABOUDI <josephaboudi32@gmail.com>
2025-04-25 11:42:19 +0100

    gestion des bugs bd

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('migrate/', views.trigger_migrate),
+
     path('register/', views.register, name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
--- users/views.py
+++ users/views.py
@@ -9,7 +9,9 @@ from django.http import JsonResponse
 from .forms import CustomUserCreationForm, CustomAuthenticationForm, UserPreferencesForm
 from .models import UserSession
 from contacts.models import Group
-
+from django.core.management import call_command
+from django.http import HttpResponse
+from django.contrib.admin.views.decorators import staff_member_required
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -88,3 +90,10 @@ def toggle_theme(request):
         'status': 'success',
         'theme': user.theme_preference
     })
+# dans views.py (temporaire)
+
+
+@staff_member_required
+def trigger_migrate(request):
+    call_command('migrate')
+    return HttpResponse("✅ Migrations exécutées avec succès !")
